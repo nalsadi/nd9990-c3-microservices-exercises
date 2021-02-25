bc06641da1ae9bdec41440830a407894eb818214
Naseem Alsadi <nalsadi@uoguelph.ca>
2021-02-25 17:23:57 -0500

    jjkdf

--- project/set_env.sh
+++ project/set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=
-export POSTGRES_PASSWORD=
-export POSTGRES_HOST=
-export POSTGRES_DB=
-export AWS_BUCKET=
-export AWS_REGION=
-export AWS_PROFILE=
-export JWT_SECRET=
+export POSTGRES_USERNAME=mydatabase
+export POSTGRES_PASSWORD=mydatabase
+export POSTGRES_HOST=mydatabase.cvhp3gbd3zo8.us-west-2.rds.amazonaws.com
+export POSTGRES_DB=mydatabase
+export AWS_BUCKET=udagram-nalsadi-dev
+export AWS_REGION=us-east-2
+export AWS_PROFILE=DEPLOYED
+export JWT_SECRET=hello
 export URL=http://localhost:8100
